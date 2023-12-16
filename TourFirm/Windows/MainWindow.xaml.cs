5df1c6ecf96258e5055da53d2df9c7ce7cdac16d
noiace <fufexl@mail.ru>
2023-12-17 00:37:53 +0300

    Added Countries

--- TourFirm/Windows/MainWindow.xaml.cs
+++ TourFirm/Windows/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using TourFirm.DataBase;
 using TourFirm.Models;
 using TourFirm.Windows;
@@ -27,7 +30,7 @@ namespace TourFirm
         public MainWindow()
         {
             InitializeComponent();
-            // var context = TourDbContext.GetContext();
+           /* // var context = TourDbContext.GetContext();
             //User user = new User() { Name = "Adel", Surname = "Galimov", Email = "a@mail.ru", Phone = "89870000123", UserName = "noiace", Password = "1234", DateOfBirth = new DateTime (2004, 9, 14).ToUniversalTime() };
 
             //context.Users.Add(user);
@@ -40,7 +43,16 @@ namespace TourFirm
             //var context = TourDbContext.GetContext();
             //Tour tour = new Tour() { Name = "Sunny Days Resort Spa", Price = 610, From = new DateTime(2023, 12, 21).ToUniversalTime(), To = new DateTime(2023, 12, 28).ToUniversalTime(), Description = "Sunny Days Resort Spa & Aqua Park находится в городе Хургада. Курортный комплекс состоит из двух частей, объединённых в один отель: Palma de Mirette (бывший отель Sunny Days Palma de Mirette) и El Palacio (бывший отель Sunny Days El Palacio). Всего в отеле 1023 номера. Отель построен в 2002 году, последняя реновация проводилась в 2021 году (реновация территории отеля, некоторых номеров категории Стандарт и Премиум). До центра города Хургада - 5 км. До Международного аэропорта Хургада (Hurghada International Airport) - 10 км.\r\n", Image = "SunnyDays.jpeg" };
             //context.Tours.Add(tour);
-            //context.SaveChanges();
+            //context.SaveChanges(); */
+            var context = TourDbContext.GetContext();
+            Tour tour = new Tour() { Name = "Страна Вано", Price = 1200, From = new DateTime(2024, 1, 14).ToUniversalTime(), To = new DateTime(2024, 2, 12).ToUniversalTime(), Image = "WanoCountry.jpg" };
+
+            //if (tour != null)
+            //{
+            //    context.Tours.Add(tour);
+            //    context.SaveChanges();
+
+            //}
         }
 
         private void Button_LogIn_Click(object sender, RoutedEventArgs e)
